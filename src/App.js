540603625abe91ff29e13f8711b54d32a0f7cd37
SamuelePassiatore <samuw.978@gmail.com>
2023-10-16 09:43:39 +0200

    updated with delete button and add button

--- src/App.js
+++ src/App.js
@@ -9,8 +9,69 @@ import dynamite from './images/dynamite.png';
 import philadelphia from './images/philadelphia.png';
 import rainbow from './images/rainbow.png';
 import shrimp from './images/shrimp.png';
+import { createRenderer } from 'react-dom/test-utils';
 
 class App extends Component {
+  state = {
+    cards: [
+      {
+        id: 0,
+        title: 'california',
+        price: 0.50,
+        image: california,
+        quantità: 0
+      },
+      {
+        id: 1,
+        title: 'dragon',
+        price: 0.50,
+        image: dragon,
+        quantità: 0
+      },
+      {
+        id: 2,
+        title: 'dynamite',
+        price: 0.50,
+        image: dynamite,
+        quantità: 0
+      },
+      {
+        id: 3,
+        title: 'philadelphia',
+        price: 0.50,
+        image: philadelphia,
+        quantità: 0
+      },
+      {
+        id: 4,
+        title: 'rainbow',
+        price: 0.50,
+        image: rainbow,
+        quantità: 0
+      },
+      {
+        id: 5,
+        title: 'shrimp',
+        price: 0.50,
+        image: shrimp,
+        quantità: 0
+      }
+    ]
+  }
+
+  deleteCard = cardId => {
+    const cards = this.state.cards.filter(card => card.id !== cardId);
+    this.setState({ cards });
+  }
+
+  handleIncrement = card => {
+    const cards = [...this.state.cards];
+    const id = cards.indexOf(card);
+    cards[id] = { ...card };
+    cards[id].quantità++;
+    this.setState({ cards });
+  }
+
   render() {
     return (
       <>
@@ -18,14 +79,16 @@ class App extends Component {
         <h1 className="p-5">First application React</h1>
         <div className="container">
           <div className="row row-cols-3">
-            <Card image={california} title="Sushi1" price={2.50} ></Card>
-            <Card image={dragon} title="Sushi1" price={2.50} ></Card>
-            <Card image={dynamite} title="Sushi1" price={2.50} ></Card>
-          </div>
-          <div className="row row-cols-3 mt-3">
-            <Card image={philadelphia} title="Sushi1" price={2.50} ></Card>
-            <Card image={rainbow} title="Sushi1" price={2.50} ></Card>
-            <Card image={shrimp} title="Sushi1" price={2.50} ></Card>
+            {this.state.cards.map(card => (
+              <Card
+                key={card.id}
+                card={card}
+                onDelete={this.deleteCard}
+                onIncrement={this.handleIncrement}
+              >
+              </Card>
+            ))
+            }
           </div>
         </div >
       </>
--- src/components/card.js
+++ src/components/card.js
@@ -4,12 +4,13 @@ class Card extends Component {
     render() {
         return (
             <div className="col">
-                <div className="card" style={{ width: "18rem" }}>
-                    <img src={this.props.image} className="card-img-top" alt="immagine_1" />
+                <div className="card mb-5" style={{ width: "18rem" }}>
+                    <button className="btn btn-primary" onClick={() => this.props.onIncrement(this.props.card)}>Aggiungi <span className='badge badge-light'>{this.props.card.quantità}</span></button>
+                    <img src={this.props.card.image} className="card-img-top" alt="immagine_1" />
                     <div className="card-body">
-                        <h5 className="card-title">{this.props.title}</h5>
-                        <p className="card-text">€{this.props.price}</p>
-                        <button className="btn btn-danger">Elimina</button>
+                        <h5 className="card-title">{this.props.card.title}</h5>
+                        <p className="card-text">€{this.props.card.price}</p>
+                        <button className="btn btn-danger" onClick={() => this.props.onDelete(this.props.card.id)}>Elimina</button>
                     </div>
                 </div>
             </div>
